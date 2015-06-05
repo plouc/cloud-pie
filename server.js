07d5fe630529fc2fa53816678d600173f70aae9d
Raphaël Benitte <benitteraphael@gmail.com>
2015-06-05 08:01:39 +0200

    Add fetch endpoint

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app     = express();
 var router  = express.Router();
+var fs      = require('fs');
+var stack   = require('./server/lib/aws/stack');
+
+var awsFilePath = 'dist/aws.json';
 
 router.use(function (req, res, next) {
     console.log('%s %s %s', req.method, req.url, req.path);
     next();
+});
+
+router.get('/fetch', function (req, res) {
+    stack.fetch()
+        .then(function (data) {
+            fs.writeFile(awsFilePath, JSON.stringify(data, null, '    '), function (err) {
+                if (err) { throw err; }
+
+                res.send(data);
+            });
+        })
+    ;
 });
 
 app.use('/', router);
